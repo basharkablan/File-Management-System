4a65e2067b453b863dc82f3a7fccc1bc062c195f
Bashar Kablan <34687794+basharkablan@users.noreply.github.com>
2019-03-01 19:58:03 +0200

    added initial code

---
+++ FileManagementSystem/Default.aspx
@@ -0,0 +1,140 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home Page</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="lib/bootstrap.min.css" />
+    <script src="lib/jquery.min.js"></script>
+    <script src="lib/bootstrap.min.js"></script>
+
+    <style>
+        * {
+            font-family: Arial;
+            font-size: 10pt;
+        }
+
+        table {
+            border: 1px solid #ccc;
+            border-collapse: collapse;
+        }
+
+        table th {
+            background-color: #F7F7F7;
+            color: #333;
+            font-weight: bold;
+        }
+
+        table th, table td {
+            padding: 5px;
+            border: 1px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="Default.aspx">File Upload System</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="Default.aspx">Home</a></li>
+
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active">
+                            <asp:LinkButton ID="LinkButton1" Visible="false" OnClick="LinkButton1_Click" runat="server">Log Out</asp:LinkButton></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <asp:Panel ID="Panel1" runat="server">
+            <div class="container">
+                <div class="row">
+                    <div id="login-part" class="col-md-4 col-md-offset-1">
+                        <h1>Login Here</h1>
+                        <br />
+                        <br />
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <asp:TextBox ID="txtusername" CssClass="form-control" runat="server"></asp:TextBox>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <asp:TextBox ID="txtpassword" TextMode="Password" CssClass="form-control" runat="server"></asp:TextBox>
+                        </div>
+                        <asp:Button ID="btnlogin" OnClick="btnlogin_Click" CssClass="btn btn-default" runat="server" Text="Login" />
+                        <br />
+                        <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+                    </div>
+                    <div id="register-part" class="col-md-4 col-md-offset-1">
+                        <h1>Register Here</h1>
+                        <br />
+                        <br />
+                        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+                        <div class="form-group">
+                            <label for="username">Username:</label>
+                            <asp:TextBox ID="txtrusrename" CssClass="form-control" runat="server"></asp:TextBox>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <asp:TextBox ID="txtrpasswrod" TextMode="Password" CssClass="form-control" runat="server"></asp:TextBox>
+                        </div>
+                        <div class="form-group">
+                            <label for="cpassword">Confirm Password:</label>
+                            <asp:TextBox ID="txtrcpassword" TextMode="Password" CssClass="form-control" runat="server"></asp:TextBox>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <asp:TextBox ID="txtemail" CssClass="form-control" runat="server"></asp:TextBox>
+                        </div>
+                        <asp:Button ID="btnRegister" OnClick="btnRegister_Click" CssClass="btn btn-default" runat="server" Text="Regisgter" />
+                        <br />
+                        <asp:Label ID="lblRegisterMessage" runat="server" Text=""></asp:Label>
+                    </div>
+                </div>
+            </div>
+        </asp:Panel>
+        <asp:Panel ID="Panel2" Visible="false" runat="server">
+            <div class="container row">
+                <div id="file-part" class="col-md-4 col-md-offset-4">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <asp:FileUpload ID="FileUpload1" CssClass="form-control" runat="server" />
+                            <br />
+                            <asp:Button ID="btnUpload" OnClick="btnUpload_Click" CssClass="btn btn-default" runat="server" Text="Upload" />
+                            <br />
+                            <asp:Label ID="lblUploadMessage" runat="server" Text=""></asp:Label>
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+                        <Columns>
+                            <asp:BoundField DataField="Name" HeaderText="File Name" />
+                            <asp:TemplateField ItemStyle-HorizontalAlign="Center">
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="lnkDownload" runat="server" Text="Download" OnClick="btnDownload_Click"
+                                        CommandArgument='<%# Eval("Id") %>'></asp:LinkButton>
+                                    <asp:LinkButton ID="lnkRemove" runat="server" Text="Remove" OnClick="btnRemove_Click"
+                                        CommandArgument='<%# Eval("Id") %>'></asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </div>
+            </div>
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ FileManagementSystem/Default.aspx.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    String CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["username"] != null)
+        {
+            LinkButton1.Visible = true;
+            Panel1.Visible = false;
+            Panel2.Visible = true;
+        }
+        else
+        {
+            LinkButton1.Visible = false;
+            Panel1.Visible = true;
+            Panel2.Visible = false;
+        }
+        if (!IsPostBack)
+        {
+            GetData();
+        }
+    }
+
+    protected void btnlogin_Click(object sender, EventArgs e)
+    {
+        if (UserAuthenitcation(txtusername.Text, txtpassword.Text))
+        {
+            Session["username"] = txtusername.Text;
+            Response.Redirect("Default.aspx");
+        }
+        else
+        {
+            lblMessage.ForeColor = Color.Red;
+            lblMessage.Text = "Invalid Username and/or Password";
+        }
+    }
+
+    private bool UserAuthenitcation(string username, string password)
+    {
+        using (SqlConnection con = new SqlConnection(CS))
+        {
+            SqlCommand cmd = new SqlCommand("sploginUser", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter paramusername = new SqlParameter("@Username", username);
+            SqlParameter parampassword = new SqlParameter("@Password", password);
+
+            cmd.Parameters.Add(paramusername);
+            cmd.Parameters.Add(parampassword);
+
+            con.Open();
+            int ReturnCode = (int)cmd.ExecuteScalar();
+            return ReturnCode == 1;
+
+        }
+    }
+
+
+    protected void btnRegister_Click(object sender, EventArgs e)
+    {
+        if (txtrusrename.Text != "" && txtrpasswrod.Text != "" && txtrcpassword.Text != "" && txtemail.Text != "")
+        {
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("spRegister", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramusername = new SqlParameter("@Username", txtrusrename.Text);
+                SqlParameter parampassword = new SqlParameter("@Password", txtrpasswrod.Text);
+                SqlParameter paramemail = new SqlParameter("@Email", txtemail.Text);
+
+                cmd.Parameters.Add(paramusername);
+                cmd.Parameters.Add(parampassword);
+                cmd.Parameters.Add(paramemail);
+
+                con.Open();
+                int ReturnCode = (int)cmd.ExecuteScalar();
+                if (ReturnCode == -1)
+                {
+                    lblRegisterMessage.ForeColor = Color.Red;
+                    lblRegisterMessage.Text = "Username is already in user please chooose another username";
+                }
+                else
+                {
+                    lblRegisterMessage.ForeColor = Color.Green;
+                    lblRegisterMessage.Text = "User Register Successfully";
+                    txtrusrename.Text = string.Empty;
+                    txtemail.Text = string.Empty;
+                }
+            }
+        }
+        else
+        {
+            lblRegisterMessage.ForeColor = Color.Red;
+            lblRegisterMessage.Text = "Please fill all fields";
+        }
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        Session.Contents.RemoveAll();
+        Response.Redirect("Default.aspx");
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            HttpPostedFile file = FileUpload1.PostedFile;
+            string fname = Path.GetFileName(FileUpload1.PostedFile.FileName);
+            string contentType = FileUpload1.PostedFile.ContentType;
+            if (file.ContentLength > 1048576)
+            {
+                Response.Write("<script>alert('File size is greater than 1MB. Please select file less than or equal to 1MB');</script>");
+            }
+            else
+            {
+                using (Stream fs = FileUpload1.PostedFile.InputStream)
+                {
+                    using (BinaryReader br = new BinaryReader(fs))
+                    {
+                        byte[] bytes = br.ReadBytes((Int32)fs.Length);
+                        using (SqlConnection con = new SqlConnection(CS))
+                        {
+                            SqlCommand cmd = new SqlCommand("Insert into tbldata values(@Name,@content,@Data,@Username)", con);
+
+                            cmd.Parameters.AddWithValue("@Name", fname);
+                            cmd.Parameters.AddWithValue("@content", contentType);
+                            cmd.Parameters.AddWithValue("@Data", bytes);
+                            cmd.Parameters.AddWithValue("@Username", Session["username"]);
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                }
+            }
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+        else
+        {
+            lblUploadMessage.ForeColor = Color.Red;
+            lblUploadMessage.Text = "Please select a file to Upload";
+        }
+
+    }
+
+    private void GetData()
+    {
+        using (SqlConnection con = new SqlConnection(CS))
+        {
+            SqlCommand cmd = new SqlCommand("Select * from tbldata where Username='" + Session["username"] + "'", con);
+            con.Open();
+            GridView1.DataSource = cmd.ExecuteReader();
+            GridView1.DataBind();
+        }
+    }
+
+    protected void btnDownload_Click(object sender, EventArgs e)
+    {
+        int id = int.Parse((sender as LinkButton).CommandArgument);
+        byte[] bytes;
+        string fileName, contentType;
+        using (SqlConnection con = new SqlConnection(CS))
+        {
+            SqlCommand cmd = new SqlCommand("Select * from tbldata where id=@Id", con);
+            cmd.Parameters.AddWithValue("@Id", id);
+            con.Open();
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                bytes = (byte[])reader["Data"];
+                contentType = reader["FileType"].ToString();
+                fileName = reader["Name"].ToString();
+            }
+        }
+        Response.Clear();
+        Response.Buffer = true;
+        Response.Charset = "";
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.ContentType = contentType;
+        Response.AppendHeader("Content-Disposition", "attachment; fileName=" + fileName);
+        Response.BinaryWrite(bytes);
+        Response.Flush();
+        Response.End();
+    }
+
+    protected void btnRemove_Click(object sender, EventArgs e)
+    {
+        int id = int.Parse((sender as LinkButton).CommandArgument);
+        byte[] bytes;
+        string fileName, contentType;
+        using (SqlConnection con = new SqlConnection(CS))
+        {
+            SqlCommand cmd = new SqlCommand("Select * from tbldata where id=@Id", con);
+            cmd.Parameters.AddWithValue("@Id", id);
+            con.Open();
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                bytes = (byte[])reader["Data"];
+                contentType = reader["FileType"].ToString();
+                fileName = reader["Name"].ToString();
+            }
+        }
+        Response.Clear();
+        Response.Buffer = true;
+        Response.Charset = "";
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.ContentType = contentType;
+        Response.AppendHeader("Content-Disposition", "attachment; fileName=" + fileName);
+        Response.BinaryWrite(bytes);
+        Response.Flush();
+        Response.End();
+    }
+}
