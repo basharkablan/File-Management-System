88c62b06e60d258543b611eaa3f0f7202b5abf9a
SafwatHalaby <5030488+SafwatHalaby@users.noreply.github.com>
2019-03-07 20:53:07 +0200

    DAL refactor

--- FileManagementSystem/Default.aspx.cs
+++ FileManagementSystem/Default.aspx.cs
@@ -4,17 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
-using System.IO;
+using System.Data.SqlClient;
 
 public partial class _Default : System.Web.UI.Page
 {
-    String CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
-
-
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Session["username"] != null)
@@ -37,7 +31,7 @@ public partial class _Default : System.Web.UI.Page
 
     protected void btnlogin_Click(object sender, EventArgs e)
     {
-        if (UserAuthenitcation(txtusername.Text, txtpassword.Text))
+        if (DataAccess.UserAuthenitcation(txtusername.Text, txtpassword.Text))
         {
             Session["username"] = txtusername.Text;
             Response.Redirect("Default.aspx");
@@ -49,64 +43,31 @@ public partial class _Default : System.Web.UI.Page
         }
     }
 
-    private bool UserAuthenitcation(string username, string password)
-    {
-        using (SqlConnection con = new SqlConnection(CS))
-        {
-            SqlCommand cmd = new SqlCommand("sploginUser", con);
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            SqlParameter paramusername = new SqlParameter("@Username", username);
-            SqlParameter parampassword = new SqlParameter("@Password", password);
-
-            cmd.Parameters.Add(paramusername);
-            cmd.Parameters.Add(parampassword);
-
-            con.Open();
-            int ReturnCode = (int)cmd.ExecuteScalar();
-            return ReturnCode == 1;
-
-        }
-    }
-
-
     protected void btnRegister_Click(object sender, EventArgs e)
     {
-        if (txtrusrename.Text != "" && txtrpasswrod.Text != "" && txtrcpassword.Text != "" && txtemail.Text != "")
+        string errorResult;
+        Color color;
+
+        if (txtrpasswrod.Text != txtrcpassword.Text)
         {
-            using (SqlConnection con = new SqlConnection(CS))
-            {
-                SqlCommand cmd = new SqlCommand("spRegister", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                SqlParameter paramusername = new SqlParameter("@Username", txtrusrename.Text);
-                SqlParameter parampassword = new SqlParameter("@Password", txtrpasswrod.Text);
-                SqlParameter paramemail = new SqlParameter("@Email", txtemail.Text);
-
-                cmd.Parameters.Add(paramusername);
-                cmd.Parameters.Add(parampassword);
-                cmd.Parameters.Add(paramemail);
-
-                con.Open();
-                int ReturnCode = (int)cmd.ExecuteScalar();
-                if (ReturnCode == -1)
-                {
-                    lblRegisterMessage.ForeColor = Color.Red;
-                    lblRegisterMessage.Text = "Username is already in user please chooose another username";
-                }
-                else
-                {
-                    lblRegisterMessage.ForeColor = Color.Green;
-                    lblRegisterMessage.Text = "User Register Successfully";
-                    txtrusrename.Text = string.Empty;
-                    txtemail.Text = string.Empty;
-                }
-            }
+            color = Color.Red;
+            errorResult = "The two passwords do not match";
         }
         else
         {
-            lblRegisterMessage.ForeColor = Color.Red;
-            lblRegisterMessage.Text = "Please fill all fields";
+            errorResult = DataAccess.register(txtrusrename.Text, txtrpasswrod.Text, txtemail.Text);
+            if (errorResult == "") // success
+            {
+                color = Color.Green;
+                lblRegisterMessage.Text = "User Registered Successfully";
+                txtrusrename.Text = string.Empty;
+                txtemail.Text = string.Empty;
+            }
+            else
+            {
+                lblRegisterMessage.Text = errorResult;
+                color = Color.Red;
+            }
         }
     }
 
@@ -118,113 +79,52 @@ public partial class _Default : System.Web.UI.Page
 
     protected void btnUpload_Click(object sender, EventArgs e)
     {
-        if (FileUpload1.HasFile)
+        string errorResult;
+        if (!FileUpload1.HasFile)
         {
-            HttpPostedFile file = FileUpload1.PostedFile;
-            string fname = Path.GetFileName(FileUpload1.PostedFile.FileName);
-            string contentType = FileUpload1.PostedFile.ContentType;
-            if (file.ContentLength > 1048576)
-            {
-                Response.Write("<script>alert('File size is greater than 1MB. Please select file less than or equal to 1MB');</script>");
-            }
-            else
-            {
-                using (Stream fs = FileUpload1.PostedFile.InputStream)
-                {
-                    using (BinaryReader br = new BinaryReader(fs))
-                    {
-                        byte[] bytes = br.ReadBytes((Int32)fs.Length);
-                        using (SqlConnection con = new SqlConnection(CS))
-                        {
-                            SqlCommand cmd = new SqlCommand("Insert into tbldata values(@Name,@content,@Data,@Username)", con);
-
-                            cmd.Parameters.AddWithValue("@Name", fname);
-                            cmd.Parameters.AddWithValue("@content", contentType);
-                            cmd.Parameters.AddWithValue("@Data", bytes);
-                            cmd.Parameters.AddWithValue("@Username", Session["username"]);
-                            con.Open();
-                            cmd.ExecuteNonQuery();
-                        }
-                    }
-
-                }
-            }
+            errorResult = "Please select a file to Upload";
+        }
+        else
+        {
+            errorResult = DataAccess.Upload(Session["username"].ToString(), FileUpload1.PostedFile);
+        }
+        
+        if (errorResult == "")
+        {
             Response.Redirect(Request.Url.AbsoluteUri);
         }
         else
         {
             lblUploadMessage.ForeColor = Color.Red;
-            lblUploadMessage.Text = "Please select a file to Upload";
+            lblUploadMessage.Text = errorResult;
         }
-
     }
 
     private void GetData()
     {
-        using (SqlConnection con = new SqlConnection(CS))
-        {
-            SqlCommand cmd = new SqlCommand("Select * from tbldata where Username='" + Session["username"] + "'", con);
-            con.Open();
-            GridView1.DataSource = cmd.ExecuteReader();
+        DataAccess.ListFilesByUser(Session["username"] + "", delegate(SqlDataReader data) {
+            GridView1.DataSource = data;
             GridView1.DataBind();
-        }
+        });
     }
 
     protected void btnDownload_Click(object sender, EventArgs e)
     {
         int id = int.Parse((sender as LinkButton).CommandArgument);
-        byte[] bytes;
-        string fileName, contentType;
-        using (SqlConnection con = new SqlConnection(CS))
-        {
-            SqlCommand cmd = new SqlCommand("Select * from tbldata where id=@Id", con);
-            cmd.Parameters.AddWithValue("@Id", id);
-            con.Open();
-            using (SqlDataReader reader = cmd.ExecuteReader())
-            {
-                reader.Read();
-                bytes = (byte[])reader["Data"];
-                contentType = reader["FileType"].ToString();
-                fileName = reader["Name"].ToString();
-            }
-        }
+        DataAccess.File file = DataAccess.GetFileById(id);
         Response.Clear();
         Response.Buffer = true;
         Response.Charset = "";
         Response.Cache.SetCacheability(HttpCacheability.NoCache);
-        Response.ContentType = contentType;
-        Response.AppendHeader("Content-Disposition", "attachment; fileName=" + fileName);
-        Response.BinaryWrite(bytes);
+        Response.ContentType = file.contentType;
+        Response.AppendHeader("Content-Disposition", "attachment; fileName=" + file.fileName);
+        Response.BinaryWrite(file.content);
         Response.Flush();
         Response.End();
     }
 
     protected void btnRemove_Click(object sender, EventArgs e)
     {
-        int id = int.Parse((sender as LinkButton).CommandArgument);
-        byte[] bytes;
-        string fileName, contentType;
-        using (SqlConnection con = new SqlConnection(CS))
-        {
-            SqlCommand cmd = new SqlCommand("Select * from tbldata where id=@Id", con);
-            cmd.Parameters.AddWithValue("@Id", id);
-            con.Open();
-            using (SqlDataReader reader = cmd.ExecuteReader())
-            {
-                reader.Read();
-                bytes = (byte[])reader["Data"];
-                contentType = reader["FileType"].ToString();
-                fileName = reader["Name"].ToString();
-            }
-        }
-        Response.Clear();
-        Response.Buffer = true;
-        Response.Charset = "";
-        Response.Cache.SetCacheability(HttpCacheability.NoCache);
-        Response.ContentType = contentType;
-        Response.AppendHeader("Content-Disposition", "attachment; fileName=" + fileName);
-        Response.BinaryWrite(bytes);
-        Response.Flush();
-        Response.End();
+        btnDownload_Click(sender, e);
     }
 }
